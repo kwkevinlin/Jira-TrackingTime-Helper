1cd0753a3f1c69a81d112d4873a43abd0f05fdac
Kevin Lin <kwkevinlin@gmail.com>
2016-11-04 21:22:33 -0700

    Added convert_to_minutes, fixed index bug

--- runner.py
+++ runner.py
@@ -5,6 +5,13 @@
 """
 
 import csv
+from pprint import pprint
+
+
+def convert_to_minutes(time_str):
+    h, m = time_str.split(':')
+    return int(h) * 60 + int(m)
+
 
 filename = "TrackingTime.csv"
 
@@ -13,19 +20,20 @@ projects_time = dict()
 with open(filename, 'rb') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        print "Project: {},\tTask: {},\tTime: {}".format(row["Project"], row["Task"], row["Duration"])
-
         # If project exists
         if row["Project"] in projects_time:
             # If Task exists
-            if row["Task"] in projects_time["Projects"]:
-                projects_time["Projects"]["Task"] += row["Duration"]
+            if row["Task"] in projects_time[row["Project"]]:
+                projects_time[row["Project"]][row["Task"]] += convert_to_minutes(row["Duration"])
             # Add new task to project
             else:
-                projects_time["Projects"]["Task"] = row["Duration"]
+                projects_time[row["Project"]][row["Task"]] = convert_to_minutes(row["Duration"])
         # Add new project to dictionary
         else:
-            projects_time[row["Project"]] = {row["Task"]: row["Duration"]}
+            projects_time[row["Project"]] = {row["Task"]: convert_to_minutes(row["Duration"])}
+
+        
+
 
 """
 End result:
